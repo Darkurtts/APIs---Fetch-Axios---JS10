bbe1644170229cf5805243ca66ce19a10db63f2a
Sean <elteemoloko@gmail.com>
2024-09-18 18:45:34 -0300

    primer commit

---
+++ palabras.js
@@ -0,0 +1,11 @@
+const words = [
+    "arena", "cafe", "celeste", "asomar", "burro", "aparte", "brisa", "actitud", "clase", "alumno",
+    "avion", "aplauso", "batir", "brillo", "boca", "bruja", "cuarzo", "andar", "ciruela", "brocha",
+    "dificil", "asiento", "aurora", "abrir", "boda", "casco", "freno", "caballo", "atmósfera", "costo",
+    "batería", "cadena", "dar", "barco", "auto", "ciego", "asilo", "calle", "ahorrar", "famoso",
+    "conjunto", "choza", "espejo", "crecer", "canguro", "dedo", "ducha", "gas", "barato", "cabeza",
+    "audaz", "cereza", "bajo", "actual", "carro", "alto", "ave", "barba", "abril", "chaleco",
+    "guitarra", "capitan", "flor", "claro", "viento", "cargar", "ver", "cuna", "cielo", "chico",
+    "agosto", "campana", "celda", "ajedrez", "amor", "cabello", "celula", "andar", "dorado", "cadena",
+    "burla", "adelante", "canela", "avanzar", "borrar", "rojo", "cena", "arena", "goma", "casa",
+    "bailar", "correr"]
